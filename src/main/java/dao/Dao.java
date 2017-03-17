16257563d6f0480b285cf766bb38a752498dc412
Koshlatyi-ML <ml.koshlatyi@gmail.com>
2017-03-17 02:50:51 +0200

    dao interfaces frame

---
+++ src/main/java/dao/Dao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+    Optional<T> find(int id);
+    List<T> findAll();
+    void create(T entity);
+    void update(T entity);
+    void delete(int id);
+}
