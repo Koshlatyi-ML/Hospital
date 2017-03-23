38c218b586e389f5a26b759285b4f1b2a912a96a
Koshlatyi-ML <ml.koshlatyi@gmail.com>
2017-03-24 00:37:43 +0200

    Patient DAO supertypes changed

--- src/main/java/dao/PatientDao.java
+++ src/main/java/dao/PatientDao.java
@@ -4,8 +4,7 @@ import domain.Patient;
 
 import java.util.List;
 
-public interface PatientDao extends CrudDao<Patient>,
-        StuffDao<Patient> {
+public interface PatientDao extends CrudDao<Patient> {
     List<Patient> findByDoctorId(long id);
     List<Patient> findByState(Patient.State state); //?
 }
